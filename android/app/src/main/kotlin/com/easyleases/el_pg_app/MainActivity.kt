6c444958b5432bde912fff8d15f96e73d22b1d99
Easyleases Admin <admin@easyleases.in>
2020-10-26 21:34:24 +0530

    pubspec entry added

--- android/app/src/main/kotlin/com/easyleases/el_pg_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/easyleases/el_pg_app/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.easyleases.el_pg_app
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+class Main
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+Activity: FlutterActivity() {
+
 }
